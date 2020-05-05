c2c87bb0aa16e779d564fe2d6199032d9c6d918d
alaaganema <45715240+alaaganema@users.noreply.github.com>
2020-05-05 18:33:25 +0300

    designing hotels page

--- ClientApp/src/app/hotel-page/hotel-page.component.ts
+++ ClientApp/src/app/hotel-page/hotel-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-hotel-page',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hotel-page.component.css']
 })
 export class HotelPageComponent implements OnInit {
+  total = 1;
+  searchFlightForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  decremantTotal() {
+    if (this.total !== 1) {
+      this.total = this.total - 1;
+      this.searchFlightForm.controls.travelers.setValue(this.total);
+    }
+  }
+  incrementTotal() {
+    if (this.total !== 8) {
+      this.total = this.total + 1;
+      this.searchFlightForm.controls.travelers.setValue(this.total);
+    }
+  }
 }
+
